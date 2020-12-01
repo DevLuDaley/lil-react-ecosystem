5c0844734e0023b9854d608f3dfb1cce6684c3d6
devludaley <daleyluis@gmail.com>
2020-12-01 16:59:04 -0500

    debug git

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NewTodoForm from './NewTodoForm';
 import TodoListItem from './TodoListItem';
+// import TodoListItem from './TodoListItem';
 import './TodoList.css';
 
 const TodoList = ({ todos = [] }) => (
