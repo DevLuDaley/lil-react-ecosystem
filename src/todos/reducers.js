ae8c8c77034d6e64074f07a492c7ae9b3e322b47
devludaley <daleyluis@gmail.com>
2020-12-01 17:31:15 -0500

    create reducers.js  && CREATE_TODO case

---
+++ src/todos/reducers.js
@@ -0,0 +1,14 @@
+import { CREATE_TODO, REMOVE_TODO } from './actions';
+
+export const todos = (state = [], action) => {
+    const { type, payload } = action;
+
+    switch (type) {
+    case CREATE_TODO: {
+        const { text } = payload;
+        const newTodo = {
+            text,
+            isCompleted: false,
+        };
+        return state.concat(newTodo);
+    }
